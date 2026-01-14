25e33b60fd00a13d3fc2aefb13f534af01e96af5
Gleb Ilinyh <gleb@ilinyh.ru>
2026-01-14 11:39:59 +0500

    feature(repo): delete test game files

--- static/js/physics.js
+++
@@ -1,6 +0,0 @@
-export const physics = {
-  speed: 4,          // скорость игрока
-  jumpVelocity: -10, // сила прыжка
-  gravity: 0.5,      // гравитация
-  terminalVy: 12     // максимальная скорость падения
-};
